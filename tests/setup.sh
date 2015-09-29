bb7d44f39e2e5a77634ae864c95afc09fe03756f
Loïc Guitaut <flink@belfalas.eu>
2015-09-29 20:06:53 +0200

    Remove linked app from links file when destroying app

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/test_helper.bash"
 
+BIN_STUBS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/bin"
+
 if [[ ! -d $DOKKU_ROOT ]]; then
   git clone https://github.com/progrium/dokku.git $DOKKU_ROOT > /dev/null
 fi
@@ -13,3 +15,10 @@ cd -
 rm -rf $DOKKU_ROOT/plugins/service
 mkdir -p $DOKKU_ROOT/plugins/service
 find ./ -maxdepth 1 -type f -exec cp '{}' $DOKKU_ROOT/plugins/service \;
+
+if [[ ! -f $BIN_STUBS/plugn ]]; then
+  wget -O- "$PLUGN_URL" | tar xzf - -C "$BIN_STUBS"
+  plugn init
+  ln -s "$DOKKU_ROOT"/plugins/* "$DOKKU_ROOT"/plugins/available
+  ln -s "$DOKKU_ROOT"/plugins/* "$DOKKU_ROOT"/plugins/enabled
+fi
